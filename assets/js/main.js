73a2564a886f43674b7405019c77a5ba71c9fae4
valekutce <43315242+valekutce@users.noreply.github.com>
2022-02-04 20:09:58 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,10 @@ $(document).ready(function (){
     let headerBars = document.querySelector('.header__bars'),
     mobileHeaderBars = document.querySelector('.mobile__header-bars'),
     faClose = document.querySelector('.fa-close'),
-    mobileMenu = document.querySelector('.mobile__menu');
+    mobileMenu = document.querySelector('.mobile__menu'),
+    headerButton = document.querySelector('.header__button'),
+    popupClose = document.querySelector('.popup__close'),
+    popupSignUp = document.querySelector('.popup__signUp');
 
     headerBars.addEventListener('click', function() {
         mobileMenu.classList.add('mobile__menu_active');
@@ -13,5 +16,12 @@ $(document).ready(function (){
         if (target && target.matches('i.fa-close')) {
             mobileMenu.classList.remove('mobile__menu_active');
         }
+    });
+    headerButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        popupSignUp.classList.add('popup__signUp_active');
+    });
+    popupClose.addEventListener('click', function() {
+        popupSignUp.classList.remove('popup__signUp_active');
     });
 });
